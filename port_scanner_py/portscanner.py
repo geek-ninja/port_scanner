6903e458a0904108cd8f9a3c4bdca32c9e4ea46e
Sanket Puhan <54413046+hacker-kyrie@users.noreply.github.com>
2019-08-22 22:55:30 +0530

    Update portscanner.py

--- port_scanner_py/portscanner.py
+++ port_scanner_py/portscanner.py
@@ -4,22 +4,15 @@ import subprocess
 import sys
 from datetime import datetime
 
-
 subprocess.call('clear', shell=True)
-
 
 remoteServer    = raw_input("Enter a remote host to scan: ")
 remoteServerIP  = socket.gethostbyname(remoteServer)
 
-
 print "-" * 60
 print "Please wait, scanning remote host", remoteServerIP
 print "-" * 60
-
-
 t1 = datetime.now()
-
-
 try:
     for port in range(1,1025):  
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,12 +32,6 @@ except socket.gaierror:
 except socket.error:
     print "Couldn't connect to server"
     sys.exit()
-
-# Checking the time again
 t2 = datetime.now()
-
-# Calculates the difference of time, to see how long it took to run the script
 total =  t2 - t1
-
-
 print 'Scanning Completed in: ', total
